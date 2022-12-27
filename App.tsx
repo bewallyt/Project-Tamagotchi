aa2a3776004de87894a7fa6134a802c561b7743f
Benson Wally Tran <bensontran@bensons-air.lan>
2022-12-27 12:08:48 -0600

    initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import 'expo-dev-client';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app! - Benson Wally Tran</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
