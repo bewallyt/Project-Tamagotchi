44e280e7fa1e5382b1d4ad690b442f9410e80ad7
Benson Wally Tran <bewallyt@gmail.com>
2022-12-28 13:12:05 -0600

    fix absolute paths
    fix absolute paths

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
+import React, { useEffect } from 'react';
 import 'expo-dev-client';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { permissions } from './src/health';
+
 export default function App() {
+  useEffect(() => {
+    console.log('mountinggggggg');
+    permissions.testHealthKit();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! - Benson Wally Tran</Text>
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+/* eslint-disable no-undef */
+const paths = {
+  health: './src/health',
+};
+
+module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.android.js', '.ios.js', '.web.js'],
+          alias: paths,
+        },
+      ],
+    ],
   };
 };
